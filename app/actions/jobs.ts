f695e3ce7b609f06de4fdff3e58ac2ca89078279
Max <73665502+maxgeor@users.noreply.github.com>
2023-09-18 11:35:00 -0400

    .

--- app/actions/jobs.ts
+++ app/actions/jobs.ts
@@ -1,8 +1,27 @@
-import * as z from "zod";
+"use server";
 
-async function onSubmit(data: z.infer<typeof formSchema>) {
-  "use server";
+import * as z from "zod";
+// import AI sdk
+
+const schema = z.object({
+  jobs: z.array(z.string()).min(2).max(2),
+  selectedJob: z.string(),
+  reason: z
+    .string()
+    .min(10, {
+      message: "Must be at least 10 characters",
+    })
+    .max(1000, {
+      message: "Must be less than 1000 characters",
+    }),
+});
+
+async function submit(data: z.infer<typeof schema>) {
+
+  console.log(data);
 
   // mutate data
   // revalidate cache
 }
+
+export { schema, submit };
